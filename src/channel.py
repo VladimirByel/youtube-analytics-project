3e02d73f82b0ff3d8b03ff8a11bbbb7fb884cc20
Vladimir Byelov <vladimir.byeloff@gmail.com>
2023-08-20 11:29:38 +0300

    all done

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,19 @@
+import requests
+import json
+import os
+
+API_KEY: str = os.getenv('YT_API_KEY')
 
 
 class Channel:
     """Класс для ютуб-канала"""
-
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
+        self.url = f"https://www.googleapis.com/youtube/v3/channels?id={channel_id}&key={API_KEY}" \
+                   f"&part=snippet,contentDetails,statistics,status"
+        self.response = requests.request('GET', url=self.url)
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        print(self.response.text)
