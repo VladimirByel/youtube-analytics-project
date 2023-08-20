f1113e6e8d406bd1209c67fe8bc45da10e7183e5
Vladimir Byelov <vladimir.byeloff@gmail.com>
2023-08-20 20:32:00 +0300

    youtube

--- src/channel.py
+++ src/channel.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import os
+from googleapiclient.discovery import build
 
 API_KEY: str = os.getenv('YT_API_KEY')
 
@@ -12,10 +13,15 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала."""
         self.channel_id = channel_id
-        self.url = f"https://www.googleapis.com/youtube/v3/channels?id={channel_id}&key={API_KEY}" \
-                   f"&part=snippet,contentDetails,statistics,status"
-        self.response = requests.request('GET', url=self.url)
+        youtube = build('youtube', 'v3', developerKey=API_KEY)
+        self.channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
 
-    def print_info(self) -> None:
-        """Выводит в консоль информацию о канале."""
-        print(self.response.text)
+
+    def printj(self) -> None:
+        """Выводит словарь в json-подобном удобном формате с отступами"""
+        print(json.dumps(self.channel, indent=2, ensure_ascii=False))
+
+
+    #def print_info(self) -> None:
+    #    """Выводит в консоль информацию о канале."""
+    #    print(self.response.text)
