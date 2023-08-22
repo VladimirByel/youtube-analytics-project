d1f94048a9f9a8a24573691ec79ce6af01ed7cfd
Vladimir Byelov <vladimir.byeloff@gmail.com>
2023-08-23 00:55:58 +0300

    got some qs

--- src/channel.py
+++ src/channel.py
@@ -1,4 +1,3 @@
-import requests
 import json
 import os
 from googleapiclient.discovery import build
@@ -9,19 +8,39 @@ API_KEY: str = os.getenv('YT_API_KEY')
 class Channel:
     """Класс для ютуб канала"""
 
-
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала."""
-        self.channel_id = channel_id
+        self.__channel_id = channel_id
         youtube = build('youtube', 'v3', developerKey=API_KEY)
-        self.channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-
+        self.channel = youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+        self.description = self.channel['items'][0]['snippet']['description']
+        self.title = self.channel['items'][0]['snippet']['title']
+        self.url = f"www.youtube.com/channel/{self.__channel_id}"
+        self.number_of_subscribers = self.channel['items'][0]['statistics']['subscriberCount']
+        self.video_count = self.channel['items'][0]['statistics']['videoCount']
+        self.number_of_views = self.channel['items'][0]['statistics']['viewCount']
 
     def printj(self) -> None:
         """Выводит словарь в json-подобном удобном формате с отступами"""
         print(json.dumps(self.channel, indent=2, ensure_ascii=False))
 
+    @staticmethod
+    def get_service():
+        youtube = Channel
+        return youtube
 
-    #def print_info(self) -> None:
-    #    """Выводит в консоль информацию о канале."""
-    #    print(self.response.text)
+    def to_json(self, name):
+        json_to_be = {
+            'id': {self.__channel_id},
+            'title': {self.title},
+            'description': {self.description},
+            'link': {self.url},
+            'subscribers': {self.number_of_subscribers},
+            'number of videos': {self.video_count},
+            'number of views': {self.number_of_views}
+        }
+        txt = str(json_to_be)
+        jsoned_dict = json.dumps(txt)
+        with open(name, 'a') as file:
+            file.write(jsoned_dict)
+
